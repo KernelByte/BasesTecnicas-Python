3b6aebfddb1c3fbcae1511c4a93c10b5ebcd1606
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-05 14:17:38 -0500

    Se añade clase sobre los strings

---
+++ 03_Strings.py
@@ -0,0 +1,20 @@
+name = "Yoniher"
+last_name = "Melendez"
+
+#Concatenacion de strings
+full_name = name +" "+ last_name
+print(full_name)
+
+# Frases y uso de apostrofes en ingles
+quote = "I'm Yoniher"
+print(quote)
+
+quote = 'she said "Hello!"'
+print(quote)
+
+# Format string py
+template = "Hola mi nombre es {} y mi apellido es {}".format(name,last_name)
+print(template)
+
+template = f"Hola mi nombre es {name} y mi apellido es {last_name}"
+print(template)
