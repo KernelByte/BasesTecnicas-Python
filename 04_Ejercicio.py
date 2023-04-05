545c9acf80c85c64c901834014d9b668e70b6f79
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-05 14:49:55 -0500

    Se añade ejercicio sobre los conceptos vistos hasta el momento

---
+++ 04_Ejercicio.py
@@ -0,0 +1,7 @@
+Mes1 = input("Ingrese su presupuesto del mes de enero: ")
+Mes2 = input("Ingrese su presupuesto del mes de febrero: ")
+Mes3 = input("Ingrese su presupuesto del mes de marzo: ")
+
+promedio = int(Mes1+Mes2+Mes3)/3
+
+print("El promedio de gastos de su presupuesto es: " + str(promedio))
