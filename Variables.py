fbceaec375c8dfa4f0c76dbc10f2c7ff70ae9fda
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-05 13:51:10 -0500

    Clase sobre el uso de variables en python

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# Variable tipo String
+nombre = "Yoniher Melendez"
+# Variable tipo numerica
+edad = 21
+print("Su nombre es: ",nombre," y su edad es: ",edad)
+
+#Reasignacion de valores.
+nombre = "Oscar Martinez"
+print("Su nombre es: ",nombre," y su edad es: ",edad)
+
+#Entrada de datos input
+nombre = input("Ingresa un nuevo nombre: ");
+print("Su nuevo nombre es: ",nombre," y su edad es: ",edad)
