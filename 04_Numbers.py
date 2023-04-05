093dd52708a258f4d3dadf04aa64efb8b6b0822e
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-05 14:30:48 -0500

    Se añade clase sobre el uso de variables numericas

---
+++ 04_Numbers.py
@@ -0,0 +1,21 @@
+#Tipo Int
+lives = 3
+print(type(lives))
+
+# Tipo Float
+temperature = 12.5
+print(type(temperature))
+
+# Se pueden hacer operaciones matematicas
+lives = 12 + 3
+lives = lives -1
+print(lives)
+
+# Decremento e incremento
+lives -= 3
+lives += 6
+print(lives)
+
+# Numeros grandes
+number = 290000000000000000000000000000.3
+print(number)
