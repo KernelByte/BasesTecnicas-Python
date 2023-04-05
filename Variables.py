e452b85dbffd8012d0c47e713b7cedc183f7c5ba
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-05 14:02:24 -0500

    Se añade explicacion sobre la funcion type para ver el tipo de las variables

--- Variables.py
+++ Variables.py
@@ -2,12 +2,19 @@
 nombre = "Yoniher Melendez"
 # Variable tipo numerica
 edad = 21
+# Variables tipo boolen
+isTrue = True
+
 print("Su nombre es: ",nombre," y su edad es: ",edad)
 
 #Reasignacion de valores.
 nombre = "Oscar Martinez"
 print("Su nombre es: ",nombre," y su edad es: ",edad)
-
+5
 #Entrada de datos input
 nombre = input("Ingresa un nuevo nombre: ");
 print("Su nuevo nombre es: ",nombre," y su edad es: ",edad)
+
+# Funcion para identificar el tipo de las variables
+print("El tipo de la variable nombre es: ", type(nombre))
+print("El tipo de la variable isTrue es: ",type(isTrue))
