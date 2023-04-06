bd5ef78666b521d58c8883a8aa2ce96ba6ce759c
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-06 13:52:02 -0500

    Clase sobre la sentencia condicional

---
+++ 12_Conditionals.py
@@ -0,0 +1,15 @@
+# Condicionales
+if (True):
+    print("Ejecuta el programa")
+else:
+   print("No ejecuta el programa")
+
+# Ejercicio
+mascota = input("Cual es su mascota favorita? ")
+
+if (mascota == "Perro") :
+    print("Los perros son lindos")
+elif (mascota == "Gato") :
+   print("Los gatos son lindos")
+else :
+    print("Esa mascota no esta disponible")
