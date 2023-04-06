eeeb16fae4bbea2d4b4c574968fcbeaae202efec
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-06 13:42:54 -0500

    Clase sobre el operadort logico NOT

---
+++ 11_OperatorNot.py
@@ -0,0 +1,7 @@
+print(not True)
+print(not False)
+
+# Operador not
+print("Operacion con not => ", not(True and True))
+print("Operacion con not => ", not(True and False))
+print(not( 5 > 3 and 3 < 6))
