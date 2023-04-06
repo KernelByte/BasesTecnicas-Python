21257e59722cb557196fbe7422a45e7ea9d3db9a
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-05 21:30:21 -0500

    Clase sobre operadores de comparacion

---
+++ 08_OperatorsCompare.py
@@ -0,0 +1,17 @@
+# Comparador mayor >
+print(5 > 4)
+
+# Comparador menor <
+print(5 < 3)
+
+# Comparador mayor o igual
+print(5 >= 5)
+
+# Comparador menor o igual
+print(5 <= 3)
+
+# Comparador igual igual
+print(5 == 7)
+
+# Comparador diferente
+print(5 != 7)
