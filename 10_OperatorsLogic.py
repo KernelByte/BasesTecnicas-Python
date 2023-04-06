3aeb22f61915a42ef44fb0ebde3b136c424921e3
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-06 13:39:38 -0500

    Clase sobre los operadores logicos AND y OR

---
+++ 10_OperatorsLogic.py
@@ -0,0 +1,9 @@
+# Operador and
+print("Operacion con and => ", True and True)
+print("Operacion con and => ", True and False)
+print( 5 > 3 and 3 < 6)
+
+# Operador or
+print("Operacion con or => ", True or True)
+print("Operacion con or => ", True or False)
+print( 5 > 3 or 3 < 6)
