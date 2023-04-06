6d14eae1b4084d3b8c0b901afea4ddc27a81b486
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-05 21:24:59 -0500

    Clase del uso de operadores matematicos

---
+++ 07_OperatorsArimetics.py
@@ -0,0 +1,36 @@
+# Sumar
+print(10 + 10)
+
+# Multiplicar
+print(10 * 10)
+
+# Dividir
+print(10 / 10)
+
+# Restar
+print(10 - 10)
+
+# Modulo
+print(10 % 3)
+
+# deja el numero entero
+print(10 // 3)
+
+# Exponencial
+print(10 ** 3)
+
+# Gerarquia de operadores
+print(10 ** 3 + 3 - 7 / 1 // 4)
+
+"""
+P - Parentesis
+E - Exponentes
+M - Multiplicacion
+D - Division
+A - Adicion
+S - Sustraccion
+"""
+
+
+# Operaciones en cadenas de texto
+print("Hola mundo" * 3)
