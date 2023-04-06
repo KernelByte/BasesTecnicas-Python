95fbe47af4ebb017d2cc6052fcf55150e19c2d87
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-05 21:08:20 -0500

    Clase sobre las transformaciones de datos

---
+++ 06_Transform.py
@@ -0,0 +1,10 @@
+name = "Oscar"
+print(type(name))
+
+# Transformacion de forma dinamica
+#name = 12
+edad = 12
+print(type(name))
+
+print(name+" "+str(edad))
+print(f"Su nombre es: {name} y su edad es {edad}")
