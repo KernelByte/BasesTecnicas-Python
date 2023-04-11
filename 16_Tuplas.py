624974751bb7990dc89697f3a7eb7b5c4d48c51b
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-10 20:24:45 -0500

    Clase sobre las tuplas y sus condiciones de uso

---
+++ 16_Tuplas.py
@@ -0,0 +1,9 @@
+# Las tuplas solo son de lectura, no pueden ser modificadas.
+
+# Tuplas de numbers
+numbers = (1,2,34,5,6,7)
+print(numbers)
+
+# Tuplas de strings
+phone = ("Samsung", "Nokia", "Motorola")
+print(phone)
