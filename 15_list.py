a4db7eefdb106c184290dc0a628a268d45caa7a3
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-10 20:17:35 -0500

    Clase sobre los metodos que tienen las listas

--- 15_list.py
+++ 15_list.py
@@ -8,3 +8,19 @@ print(phone)
 
 # Operaciones en las listas
 print("Motorola" in phone)
+
+""" 
+-- TODO: Metodos para las listas:
+
+append(valor): Añade un ítem al final de la lista.
+insert(posicion, valor): Agrega un ítem a la lista en un índice específico.
+index(valor): Devuelve el índice en el que aparece un ítem (error si no aparece).
+remove(valor): Borra el primer ítem de la lista cuyo valor concuerde con el que indicamos.
+pop(): Extrae el ultimo ítem de la lista y lo borra. Tambien se puede enviar la posicion para que sea mas exacto
+reverse(): Le da la vuelta a la lista actual.
+sort(): Ordena automáticamente los ítems de una lista por su valor de menor a mayor.
+clear(): Vacía todos los ítems de una lista.
+extend(): Une una lista a otra.
+count(): Cuenta el número de veces que aparece un ítem.
+
+"""
