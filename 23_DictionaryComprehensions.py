757b787ba5eaacfe22d1193a2e0fc497d1cc3b29
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-11 13:34:19 -0500

    Se mejora la clase de los diccionarios

--- 23_DictionaryComprehensions.py
+++ 23_DictionaryComprehensions.py
@@ -6,3 +6,8 @@ print(dict2)
 
 new_dict = {name:age for (name,age) in zip(names,ages)}
 print(new_dict)
+
+# Dictionary condictions
+text = "Hola a todos, esta es una cadena de texto de prueba."
+unique = { c: text.count(c) for c in text if c in 'aeiou' }
+print(unique)
