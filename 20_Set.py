a77ae3a5a6872e25fb1bded99178c82788eff0be
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-11 11:18:03 -0500

    Clase sobre los diferentes metodos CRUD para los conjuntos SET en python

--- 20_Set.py
+++ 20_Set.py
@@ -22,6 +22,45 @@ print (set_from_tuples) # {'as', 'abc', 'cbv'}
 numbers = [1,2,3,1,2,3,4]
 set_numbers= set(numbers)
 print (set_numbers) # {1, 2, 3, 4}
+
 # si quiero convertir este set único a una lista, lo puedo hacer:
 unique_numbers = list(set_numbers)
 print (unique_numbers)
+
+#len() : Devuelve el tamaño del conjunto
+size = len(set_countries)
+print(size)
+
+#in, permite sabes si un elemento se encuentra enel conjunto, la expresión se evalua como true si el elemento se encuentra enel conjunto y false si el elemento nose encuentra enel conjunto
+print('col' in set_countries)
+print('pe' in set_countries)
+
+# add(): Añade un elemento al conjunto.
+set_countries.add('pe')
+print(set_countries)
+set_countries.add('pe')
+print(set_countries)
+
+# update(): Añade cualquier tipo de objeto iterable como: listas, tuplas
+set_countries.update({'ar', 'ecua', 'pe'})
+print(set_countries)
+
+# remove(): Elimina un elemento y si este no existe lanza el error “keyError”
+set_countries.remove('col')
+print(set_countries)
+set_countries.remove('ar')
+
+#discard(): Elimina un elemento y si ya existe no lanza ningún error
+set_countries.discard('arg')
+print(set_countries)
+set_countries.add('arg')
+print(set_countries)
+
+#pop(): Nos devuelve un elemento aleatorio ylo elimina y si el conjunto está vacío lanza el error “key error”.
+print(set_countries.pop())
+print(set_countries)
+
+#clear(): Elimina todo el contenido del conjunto
+set_countries.clear()
+print(set_countries)
+print(len(set_countries))
