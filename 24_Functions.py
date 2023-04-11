bc965e12d3d6b9a8b339a31ebc87ba0d1342f01b
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-11 15:22:01 -0500

    VClase de valores por defecto dentro de las funciones y return de multiples valores

--- 24_Functions.py
+++ 24_Functions.py
@@ -1,5 +1,13 @@
 # Definicion de una funcion
-def suma(a,b):
+# Agregamos valores por defecto
+def suma(a = 1,b = 2):
     return print("La suma de los numeros es: ",a+b)
 
 suma(2,8)
+
+# Retornar mas de un valor
+def mensaje():
+    return 34,2,"Hola"
+
+resultado = mensaje()
+print(resultado)
