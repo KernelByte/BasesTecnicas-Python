b3373ad3315f2d2cb776fcedb9e09c9b40a8ba36
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-10 19:58:48 -0500

    Clase sobre las listas

---
+++ 15_list.py
@@ -0,0 +1,10 @@
+# list of numbers
+numbers = [2,3,4,5,6,7,8,7]
+print(numbers)
+
+# list of strings
+phone = ["Samsung","Nokia","Motorola"]
+print(phone)
+
+# Operaciones en las listas
+print("Motorola" in phone)
