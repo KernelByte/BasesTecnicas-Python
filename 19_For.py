905506d4164e1fedc36120996c2a844247e39e79
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-10 21:26:45 -0500

    clase sobre la sentencia for en python

---
+++ 19_For.py
@@ -0,0 +1,17 @@
+for elemento in range(1,7):
+    print("Mensaje # ",elemento)
+    # Código a ejecutar para cada elemento en la secuencia
+
+
+list = [23,34,2,4,56,3]
+for elemento2 in list :
+    print("Mensaje # ",elemento2)
+
+Person = {"name": "Yoniher Melendez",
+          "age": 20,
+          "country": "Colombia"
+          }
+
+for key , value in Person.items() :
+    print("key", "=>", key)
+    print("value", "=>", value)
