daab5dc99d49aec5bc2486d19418d2851df16af8
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-10 21:15:09 -0500

    Clase sobre ciclo while en python

---
+++ 18_While.py
@@ -0,0 +1,9 @@
+# Condicion de ejecucion
+contador = 1;
+
+while (contador <= 5):
+    print("Mensaje #",contador) # Código a ejecutar mientras se cumpla la condición
+    contador += 1
+    if (contador == 3) : # Condicion forzada del while
+        break;
+      # continue - instrucion para pasar a realizar otra instruccion diferente
