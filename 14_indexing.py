5a2b994bba73e165680abc2e06c6e7afb48bfc05
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-10 19:27:59 -0500

    Clase sobre indixes en los strings

---
+++ 14_indexing.py
@@ -0,0 +1,35 @@
+# Todas las cadenas tienen un index de su posicion en memoria
+text = "Ella sabe programar en python"
+"""
+print(text[0])
+print(text[1])
+print(text[2])
+print(text[3])
+print(text[4])
+print(text[5])
+print(text[6])
+print(text[7])
+print(text[8])
+print(text[9])
+print(text[10])
+print(text[11])
+print(text[12])
+print(text[13])
+print(text[14])
+print(text[15])
+print(text[16])
+print(text[17])
+print(text[18])
+print(text[19])
+print(text[20])
+"""
+
+# Como saber el ultimo caracter de un texto
+print(text[len(text)-1])
+print(text[-1])
+
+# Slicing - Forma de sacar cadenas de texto desde posiciones
+print(text[0:5])
+print(text[:5])
+print(text[5:])
+print(text[5:10:2])
