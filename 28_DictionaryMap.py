6f04d12866622c60348f4e068a76e47f40dc5b44
Yoniher Melendez <yonihermelendez@gmail.com>
2023-04-17 10:22:50 -0500

    Se da solucion a problema de modidficacion a los diccionarios por su referencia en memoria!

--- 28_DictionaryMap.py
+++ 28_DictionaryMap.py
@@ -21,8 +21,10 @@ print(prices)
 # Conversion de diccionario añadiendo nuevo campo
 
 def convert (values) :
-    values["taxes"] = values["price"] * 0.19
-    return values
+    # Se realiza copia del diccionario original para no alterar al original pór su referencia en memoria
+    new_items = values.copy();
+    new_items["taxes"] = new_items["price"] * 0.19
+    return new_items
 
 new_list = list(map(convert,items))
 print(new_list)
