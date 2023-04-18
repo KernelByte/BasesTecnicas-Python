eb4f73a41afd362857af6a0226b0eadfc7f552da
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-17 22:52:22 -0500

    Se añade explicacion de filter con los diccionarios

--- 29_Filter.py
+++ 29_Filter.py
@@ -4,3 +4,24 @@ numbers = [1,2,3,4,5,6,7,8]
 # Se aplica filtro haciendo uso de una funcion
 new_numbers = list(filter(lambda x : x % 2 == 0, numbers))
 print(new_numbers)
+
+# Filtro lista de diccionarios
+persons = [
+    {
+        "name" : "Yoniher",
+        "age" : 23
+    },
+
+    {
+        "name" : "Oscar Martinez",
+        "age" : 34
+    },
+
+    {
+        "name" : "Luis mario",
+        "age" : 56
+    }
+]
+
+new_persons = list(filter(lambda x : x["age"] > 30, persons))
+print(new_persons)
