c41a62950c5ea22955244ce2ba536284b1d3ab09
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-04-18 23:06:17 -0500

    Clase Duabilidad en los modulos para ejecutarse como script o como archivo py

--- app/main.py
+++ app/main.py
@@ -14,7 +14,12 @@ data = [{
 }
 ]
 
-country = input('Type Country => ')
+def run():
+   country = input('Type Country => ')
 
-result = utils.population_by_country(data, country)
-print(result)
+   result = utils.population_by_country(data, country)
+   print(result)
+
+
+if __name__ == "__main__":
+	run()
