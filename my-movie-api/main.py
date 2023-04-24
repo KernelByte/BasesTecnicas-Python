0377c2510696ef5f2e3c82d55c697c7a02270002
KernelByte <yonihermelendez@gmail.com>
2023-04-24 17:49:05 -0500

    Documentacion con el comando Docs en FastAPI

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "My movie app"
+app.version = 1.0
 
-@app.get("/")
-def get_init():
+@app.get("/",tags=["Home url"])
+def get_message():
     return "Hello world"
+
+
+@app.get("/person",tags=["Person url"])
+def get_person():
+    return [
+        {
+            "name" : "Yoniher Melendez",
+            "age"  : 23
+        },
+
+        {
+            "name" : "Oscar Martinez",
+            "age"  : 34
+        }
+    ]
