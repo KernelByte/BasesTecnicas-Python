40a3dccb0d61af5fa6c4ab7fa8dfe92e6bf64011
KernelByte <yonihermelendez@gmail.com>
2023-04-26 11:23:25 -0500

    Validacion de esquemas con la clase Field

--- my-movie-api/Esquemas.py
+++ my-movie-api/Esquemas.py
@@ -1,7 +1,16 @@
 # Metodo para la creacion de esquema de datos
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Movie(BaseModel):
     id: int | None = None
-    name: str 
-    category: str
+    name: str = Field(default="Ingresa nombre", max_length=200)
+    category: str = Field(default="Ingresa la categoria")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "Oscar Martinez",
+                "category": "Action"
+            }
+        }
