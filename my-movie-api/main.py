261192bf96b6e67a7f2b6b52a979778ed1132e26
KernelByte <yonihermelendez@gmail.com>
2023-04-28 11:14:59 -0500

    Se termina el CRUD del API REST con modificacion y delete

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -82,7 +82,7 @@ def get_movies_by_category(categories: str = Query(min_length=5)):
      return list(filter(lambda movie : movie["category"] == categories, movies))
 '''
 
-
+# Metodo GET - TODOS LOS REGISTROS
 @app.get("/movies", tags=["movies"], response_model=list[Esquemas.Movie], status_code=200)
 def get_all_movies() -> list[Esquemas.Movie]:
     db = Session()
@@ -90,6 +90,8 @@ def get_all_movies() -> list[Esquemas.Movie]:
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 
+
+# Metodo GET - UN SOLO REGISTRO
 @app.get("/movies/{id}", tags=["movies"], dependencies=[Depends(JWTBearer)])
 def get_movies(id: int = Path(ge=1, le=200)):
     db = Session()
@@ -98,9 +100,9 @@ def get_movies(id: int = Path(ge=1, le=200)):
         return JSONResponse(status_code=404, content={"message": "Movie no encontrada"})
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
+
+
 # Metodo POST
-
-
 @app.post("/movies", tags=["movies"], status_code=201)
 def create_movie(movie: Esquemas.Movie) -> dict:
     db = Session()
@@ -109,30 +111,40 @@ def create_movie(movie: Esquemas.Movie) -> dict:
     db.commit()
     return JSONResponse(status_code=201, content={"message": "Se ha registrado la pelicula de manera correcta"})
 
+
+
 # Metodo PUT
-
-
 @app.put("/movies/{id}", tags=["movies"])
-def update_movies(id: int, movie: Esquemas.Movie):
-    for item in movies:
-        if item["id"] == id:
-            item["name"] = movie.name
-            item["category"] = movie.category
+def update_movies(id: int, movie: Esquemas.Movie) -> dict:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content={"message": "Movie no encontrada"})
+    result.title = movie.title
+    result.overview = movie.overview
+    result.year =  movie.year
+    result.rating = movie.rating
+    result.category = movie.category
+    db.commit()
     return JSONResponse(content={"message": "Se a modificado la pelicula"})
 
+
+
 # Metodo DELETE
-
-
 @app.delete("/movies/{id}", tags=["movies"])
-def delete_movie(id: int):
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-    return movies
+def delete_movie(id: int) -> dict:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content={"message": "Movie no encontrada"})
+    else:
+       db.delete(result)
+       db.commit()
+       return JSONResponse(content={"message": "Se a eliminado la pelicula"})
+
+
 
 # EndPoint de LOGIN
-
-
 @app.post("/login", tags=["Login"])
 def login(user: Esquemas.User):
 
