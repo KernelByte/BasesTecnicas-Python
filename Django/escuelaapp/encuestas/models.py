8edce7b650aa94eb4374706884bd99ad69203a05
KernelByte <yonihermelendez@gmail.com>
2023-04-30 13:53:39 -0500

    Se crea el modelo de las tablas a usar y se corre la primera migracion de la base de datos

--- Django/escuelaapp/encuestas/models.py
+++ Django/escuelaapp/encuestas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    #Columnas de la tabla
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField("date published")
+
+class Choises(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
